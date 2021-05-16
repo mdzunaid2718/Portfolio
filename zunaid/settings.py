6b4199fff16d1ad0b8090d536d2a8381242a7c98
mdzunaid2718 <84236493+mdzunaid2718@users.noreply.github.com>
2021-05-16 07:41:18 -0400

    Update settings.py

--- zunaid/settings.py
+++ zunaid/settings.py
@@ -108,8 +108,8 @@ STATICFILES_DIRS =[
 # Send Mail settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zunaid2718@gmail.com'
-EMAIL_HOST_PASSWORD = 'zgwv vqaq ekix lqtq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Default From Email'
